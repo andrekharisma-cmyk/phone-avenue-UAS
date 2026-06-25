b809ee8975345c67f487fa59faafe07225270eb4
fransdinata <fransedinata@gmail.com>
2026-06-26 00:28:33 +0700

    Fix linting errors with RuboCop

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :products, only: [:index, :show]
-      resources :users,    only: [:create]
-      resources :orders,   only: [:create]
+      resources :products, only: [ :index, :show ]
+      resources :users,    only: [ :create ]
+      resources :orders,   only: [ :create ]
     end
   end
 
   # Health check
-  get "/up", to: proc { [200, {}, ["OK"]] }
+  get "/up", to: proc { [ 200, {}, [ "OK" ] ] }
 end
