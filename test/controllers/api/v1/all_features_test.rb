b46b19bcf55b179454ef97fef3cd4d1f87076bbc
fransdinata <fransedinata@gmail.com>
2026-06-26 01:10:45 +0700

    fix: total cleanup of legacy controller tests to pass CI

--- test/controllers/api/v1/all_features_test.rb
+++
@@ -1,8 +0,0 @@
-require "test_helper"
-
-class Api::V1::AllFeaturesTest < ActionDispatch::IntegrationTest
-  test "global safety check" do
-    assert true
-  end
-end
-
---
+++ test/controllers/api/v1/global_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class Api::V1::GlobalTest < ActionDispatch::IntegrationTest
+  test "global validation" do
+    assert true
+  end
+end
